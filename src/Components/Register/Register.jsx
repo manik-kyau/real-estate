4b874866ee07aa728aed44e9964754f95e779946
manik-kyau <you@example.commaniksarkar.kyau.cse>
2024-04-09 06:08:30 -0700

    complate Registration page design

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,7 +1,45 @@
+import { Link } from "react-router-dom";
+
 const Register = () => {
+
+    const handleRegistrationForm = e => {
+        e.preventDefault();
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const photo = e.target.photo.value;
+        const password = e.target.password.value;
+        console.log(name);
+        console.log(email);
+        console.log(photo);
+        console.log(password);
+    }
     return (
-        <div>
-            register page
+        <div className="">
+            <section className="p-6 dark:text-gray-800">
+                <form onSubmit={handleRegistrationForm} className="container w-full max-w-xl p-8 mx-auto space-y-6 rounded-md shadow-2xl dark:bg-gray-50 border border-red-600">
+                    <h2 className="w-full text-3xl font-bold leading-tight">Registration Now</h2>
+                    <div>
+                        <label className="block text-lg font-semibold">Name</label>
+                        <input type="text" name='name' placeholder="Your name" required className="block w-full p-2 rounded focus:outline-none focus:ring focus:ring-opacity-25 focus:dark:ring-violet-600 dark:bg-gray-100 border" />
+                    </div>
+                    <div>
+                        <label className="block text-lg font-semibold">Email</label>
+                        <input type="email" name='email' placeholder="Your email" required className="block w-full p-2 rounded focus:outline-none focus:ring focus:ring-opacity-25 focus:dark:ring-violet-600 dark:bg-gray-100 border" />
+                    </div>
+                    <div>
+                        <label className="block text-lg font-semibold">Email</label>
+                        <input type="file" name='photo' placeholder="Your email" required className="block w-full p-2 rounded focus:outline-none focus:ring focus:ring-opacity-25 focus:dark:ring-violet-600 dark:bg-gray-100 border" />
+                    </div>
+                    <div>
+                        <label className="block text-lg font-semibold">Password</label>
+                        <input type="password" name='password' placeholder="Your email" required className="block w-full p-2 rounded focus:outline-none focus:ring focus:ring-opacity-25 focus:dark:ring-violet-600 dark:bg-gray-100 border" />
+                    </div>
+                    <div>
+                        <input type="submit" value='Register' className="block w-full p-2 rounded focus:dark:ring-violet-600 bg-green-500 text-lg text-white font-semibold" />
+                    </div>
+                        <p className="text-base dark:text-gray-600">Already have an account? Please <Link to='/login' className="focus:underline hover:underline font-bold ml-1">Log In</Link></p>
+                </form>
+            </section>
         </div>
     );
 };
